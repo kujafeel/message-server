cbf575dd24503954b63db86fd1cbc04f3c9d6355
kujafeel <kujafeel@gmail.com>
2025-03-10 17:13:36 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 application {
-    mainClass.set("MainKt") // entry point
+    mainClass.set("MainKt") // 메인 함수가 있는 파일 이름.kt 기준
 }
 
 repositories {
@@ -16,4 +16,19 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:2.3.4")
     implementation("io.ktor:ktor-server-websockets:2.3.4")
     implementation("com.microsoft.sqlserver:mssql-jdbc:12.4.2.jre11")
+}
+
+tasks.register<Jar>("fatJar") {
+    archiveBaseName.set("message-server")
+    archiveClassifier.set("all")
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(
+        configurations.runtimeClasspath.get().map {
+            if (it.isDirectory) it else zipTree(it)
+        }
+    )
+    with(tasks.jar.get() as CopySpec)
 }
